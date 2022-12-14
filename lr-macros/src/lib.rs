104f13468ce336f7a10bd0c9784d1377da7a98e5
Nate Catelli <ncatelli@packetfire.org>
2022-12-14 01:58:37 +0000

    wip: proc macros

---
+++ lr-macros/src/lib.rs
@@ -0,0 +1,8 @@
+use proc_macro2::{Span, TokenStream};
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(lrSymbol, attributes(terminal, nonterminal))]
+pub fn symbol_token_gen(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let _input = parse_macro_input!(input as DeriveInput);
+    todo!()
+}
