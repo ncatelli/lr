ac5c3dcf194b19e7f988b033454503192372e3b7
Nate Catelli <ncatelli@packetfire.org>
2023-04-10 02:46:41 +0000

    clean up tests to match external test case

--- lr-tests/benches/simple_expression_parsing.rs
+++ lr-tests/benches/simple_expression_parsing.rs
@@ -1,69 +1,89 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-
 use lr_core::{TerminalOrNonTerminal, TerminalRepresentable};
 pub use lr_derive::Lr1;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum TerminalKind {
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Int,
+    Eof,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Terminal {
     Plus,
-    #[allow(unused)]
+    Minus,
     Star,
-    Zero,
-    One,
+    Slash,
+    Int(i64),
     Eof,
 }
 
 impl std::fmt::Display for Terminal {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let repr = match self {
-            Terminal::Plus => "+",
-            Terminal::Star => "*",
-            Terminal::Zero => "0",
-            Terminal::One => "1",
-            Terminal::Eof => "<$>",
-        };
-
-        write!(f, "{}", repr)
+        match self {
+            Self::Plus => write!(f, "+"),
+            Self::Minus => write!(f, "-"),
+            Self::Star => write!(f, "*"),
+            Self::Slash => write!(f, "/"),
+            Self::Int(i) => write!(f, "{}", i),
+            Self::Eof => write!(f, "<$>"),
+        }
     }
 }
 
 impl TerminalRepresentable for Terminal {
     /// the associated type representing the variant kind.
-    type Repr = Self;
+    type Repr = TerminalKind;
 
     fn eof() -> Self::Repr {
         Self::Repr::Eof
     }
 
     fn to_variant_repr(&self) -> Self::Repr {
-        *self
+        match self {
+            Self::Plus => Self::Repr::Plus,
+            Self::Minus => Self::Repr::Minus,
+            Self::Star => Self::Repr::Star,
+            Self::Slash => Self::Repr::Slash,
+            Self::Int(_) => Self::Repr::Int,
+            Self::Eof => Self::Repr::Eof,
+        }
     }
 }
 
 #[derive(Debug, PartialEq)]
-pub enum NonTermKind {
+pub enum MultiplicativeTermKind {
     Mul(NonTerminal, NonTerminal),
+    Div(NonTerminal, NonTerminal),
+    Unary(NonTerminal),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum AdditiveTermKind {
     Add(NonTerminal, NonTerminal),
+    Sub(NonTerminal, NonTerminal),
     Unary(NonTerminal),
 }
 
 type TermOrNonTerm = TerminalOrNonTerminal<Terminal, NonTerminal>;
 
 #[allow(unused)]
-fn reduce_b_non_term(elems: &mut Vec<TermOrNonTerm>) -> Result<NonTerminal, String> {
+fn reduce_primary(elems: &mut Vec<TermOrNonTerm>) -> Result<NonTerminal, String> {
     if let Some(TermOrNonTerm::Terminal(term)) = elems.pop() {
-        Ok(NonTerminal::B(term))
+        Ok(NonTerminal::Primary(term))
     } else {
         Err("expected terminal at top of stack in reducer.".to_string())
     }
 }
 
 #[allow(unused)]
-fn reduce_e_unary_non_term(elems: &mut Vec<TermOrNonTerm>) -> Result<NonTerminal, String> {
+fn reduce_expr_unary(elems: &mut Vec<TermOrNonTerm>) -> Result<NonTerminal, String> {
     if let Some(TermOrNonTerm::NonTerminal(nonterm)) = elems.pop() {
-        let non_term_kind = NonTermKind::Unary(nonterm);
-
-        Ok(NonTerminal::E(Box::new(non_term_kind)))
+        Ok(NonTerminal::Expr(Box::new(nonterm)))
     } else {
         Err(format!(
             "expected non-terminal at top of stack in production 3 reducer.",
@@ -72,7 +92,33 @@ fn reduce_e_unary_non_term(elems: &mut Vec<TermOrNonTerm>) -> Result<NonTerminal
 }
 
 #[allow(unused)]
-fn reduce_e_binary_non_term(
+fn reduce_multiplicative_unary(elems: &mut Vec<TermOrNonTerm>) -> Result<NonTerminal, String> {
+    if let Some(TermOrNonTerm::NonTerminal(nonterm)) = elems.pop() {
+        let non_term_kind = MultiplicativeTermKind::Unary(nonterm);
+
+        Ok(NonTerminal::Multiplicative(Box::new(non_term_kind)))
+    } else {
+        Err(format!(
+            "expected non-terminal at top of stack in production 3 reducer.",
+        ))
+    }
+}
+
+#[allow(unused)]
+fn reduce_additive_unary(elems: &mut Vec<TermOrNonTerm>) -> Result<NonTerminal, String> {
+    if let Some(TermOrNonTerm::NonTerminal(nonterm)) = elems.pop() {
+        let non_term_kind = AdditiveTermKind::Unary(nonterm);
+
+        Ok(NonTerminal::Additive(Box::new(non_term_kind)))
+    } else {
+        Err(format!(
+            "expected non-terminal at top of stack in production 3 reducer.",
+        ))
+    }
+}
+
+#[allow(unused)]
+fn reduce_multiplicative_binary(
     production_id: usize,
     elems: &mut Vec<TermOrNonTerm>,
 ) -> Result<NonTerminal, String> {
@@ -89,13 +135,40 @@ fn reduce_e_binary_non_term(
         [optional_lhs, optional_term, optional_rhs]
     {
         let non_term_kind = match op {
-            Terminal::Star => NonTermKind::Mul(lhs, rhs),
-            Terminal::Plus => NonTermKind::Add(lhs, rhs),
+            Terminal::Star => MultiplicativeTermKind::Mul(lhs, rhs),
+            Terminal::Slash => MultiplicativeTermKind::Div(lhs, rhs),
             // Dispatcher should never reach this block of code due to parser guarantees.
             _ => unreachable!(),
         };
 
-        Ok(NonTerminal::E(Box::new(non_term_kind)))
+        Ok(NonTerminal::Multiplicative(Box::new(non_term_kind)))
+    } else {
+        Err(err_msg)
+    }
+}
+
+#[allow(unused)]
+fn reduce_additive_binary(elems: &mut Vec<TermOrNonTerm>) -> Result<NonTerminal, String> {
+    let optional_rhs = elems.pop();
+    let optional_term = elems.pop();
+    let optional_lhs = elems.pop();
+    let err_msg = format!(
+        "expected 3 elements at top of stack in production  reducer. got [{:?}, {:?}, {:?}]",
+        &optional_lhs, &optional_term, &optional_rhs
+    );
+
+    // reversed due to popping elements
+    if let [Some(TermOrNonTerm::NonTerminal(lhs)), Some(TermOrNonTerm::Terminal(op)), Some(TerminalOrNonTerminal::NonTerminal(rhs))] =
+        [optional_lhs, optional_term, optional_rhs]
+    {
+        let non_term_kind = match op {
+            Terminal::Plus => AdditiveTermKind::Add(lhs, rhs),
+            Terminal::Minus => AdditiveTermKind::Sub(lhs, rhs),
+            // Dispatcher should never reach this block of code due to parser guarantees.
+            _ => unreachable!(),
+        };
+
+        Ok(NonTerminal::Additive(Box::new(non_term_kind)))
     } else {
         Err(err_msg)
     }
@@ -103,47 +176,85 @@ fn reduce_e_binary_non_term(
 
 #[derive(Debug, Lr1, PartialEq)]
 pub enum NonTerminal {
-    #[goal(r"<E>", reduce_e_unary_non_term)]
-    #[production(r"<E> Terminal::Star <B>", |elems| { reduce_e_binary_non_term(2, elems) })]
-    #[production(r"<E> Terminal::Plus <B>", |elems| { reduce_e_binary_non_term(3, elems) })]
-    #[production(r"<B>", reduce_e_unary_non_term)]
-    E(Box<NonTermKind>),
-    #[production(r"Terminal::Zero", reduce_b_non_term)]
-    #[production(r"Terminal::One", reduce_b_non_term)]
-    B(Terminal),
+    #[goal(r"<Expr>", reduce_expr_unary)]
+    #[production(r"<Additive>", reduce_expr_unary)]
+    Expr(Box<NonTerminal>),
+    #[production(r"<Additive> Terminal::Plus <Multiplicative>", reduce_additive_binary)]
+    #[production(r"<Additive> Terminal::Minus <Multiplicative>", reduce_additive_binary)]
+    #[production(r"<Multiplicative>", reduce_additive_unary)]
+    Additive(Box<AdditiveTermKind>),
+    #[production(r"<Multiplicative> Terminal::Star <Primary>", |elems| { reduce_multiplicative_binary(6, elems) })]
+    #[production(r"<Multiplicative> Terminal::Slash <Primary>", |elems| { reduce_multiplicative_binary(7, elems) })]
+    #[production(r"<Primary>", reduce_multiplicative_unary)]
+    Multiplicative(Box<MultiplicativeTermKind>),
+    #[production(r"Terminal::Int", reduce_primary)]
+    Primary(Terminal),
 }
 
-fn generate_test_stream(len: usize) -> Vec<Terminal> {
-    let fill_to = [[Terminal::Plus, Terminal::Zero]]
-        .into_iter()
-        .cycle()
-        .take(len)
-        .flatten();
+fn parse_basic_expression(c: &mut Criterion) {
+    let mut group = c.benchmark_group("simple calculator expression parsing");
 
-    [Terminal::One]
-        .into_iter()
-        .chain(fill_to)
-        .chain([Terminal::Eof].into_iter())
-        .collect()
-}
+    group.bench_function("without tokenization", |b| {
+        let token_stream = [
+            Terminal::Int(10),
+            Terminal::Slash,
+            Terminal::Int(5),
+            Terminal::Plus,
+            Terminal::Int(1),
+            Terminal::Eof,
+        ];
 
-fn fixed_size_expression(c: &mut Criterion) {
-    let input = generate_test_stream(1);
+        let expected = NonTerminal::Expr(Box::new(NonTerminal::Additive(Box::new(
+            AdditiveTermKind::Add(
+                NonTerminal::Additive(Box::new(AdditiveTermKind::Unary(
+                    NonTerminal::Multiplicative(Box::new(MultiplicativeTermKind::Div(
+                        NonTerminal::Multiplicative(Box::new(MultiplicativeTermKind::Unary(
+                            NonTerminal::Primary(Terminal::Int(10)),
+                        ))),
+                        NonTerminal::Primary(Terminal::Int(5)),
+                    ))),
+                ))),
+                NonTerminal::Multiplicative(Box::new(MultiplicativeTermKind::Unary(
+                    NonTerminal::Primary(Terminal::Int(1)),
+                ))),
+            ),
+        ))));
 
-    c.bench_function("fixed size input", |b| {
+        let expected = Ok(expected);
+
         b.iter(|| {
-            let tokenizer = black_box((&input).iter().copied());
-
-            let parse_tree = lr_parse_input(tokenizer);
-            let expected = NonTerminal::E(Box::new(NonTermKind::Add(
-                NonTerminal::E(Box::new(NonTermKind::Unary(NonTerminal::B(Terminal::One)))),
-                NonTerminal::B(Terminal::Zero),
-            )));
-
-            assert_eq!(parse_tree, Ok(expected))
-        })
+            let parse_tree = lr_parse_input(black_box((&token_stream).iter().copied()));
+            assert_eq!(&parse_tree, &expected);
+        });
     });
 }
 
-criterion_group!(benches, fixed_size_expression);
+fn parse_large_expression(c: &mut Criterion) {
+    let mut group = c.benchmark_group("large expression");
+    let token_stream = [Terminal::Int(10)]
+        .into_iter()
+        .chain(
+            [
+                [Terminal::Slash, Terminal::Int(5)],
+                [Terminal::Plus, Terminal::Int(1)],
+                [Terminal::Minus, Terminal::Int(2)],
+                [Terminal::Star, Terminal::Int(6)],
+            ]
+            .into_iter()
+            .cycle()
+            .take(100)
+            .flatten(),
+        )
+        .chain([Terminal::Eof])
+        .collect::<Vec<_>>();
+
+    group.bench_function("without tokenization", |b| {
+        b.iter(|| {
+            let parse_tree = lr_parse_input(black_box((&token_stream).iter().copied()));
+            assert!(parse_tree.is_ok());
+        });
+    });
+}
+
+criterion_group!(benches, parse_basic_expression, parse_large_expression);
 criterion_main!(benches);
